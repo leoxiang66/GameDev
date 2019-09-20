59b7df8ff18e7bb6551c7e75a3beaf4410da78be
tao <ge25pof@mytum.de>
2019-09-20 23:48:47 +0200

    rendered the surface, now it's white

--- main.cpp
+++ main.cpp
@@ -28,10 +28,22 @@ int main(int argc, char* args[]) {
             screenSurface= SDL_GetWindowSurface(window);
 
             // fill the surface white
-            SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,0xFF,0xFF,0xFF));
+            SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,0x0,0x0,0x0));
 
             // update the surface
             SDL_UpdateWindowSurface(window);
+
+            SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+            for(bool keep_running = true; keep_running;) {
+                for(SDL_Event ev; SDL_PollEvent(&ev);) {
+                    if(ev.type == SDL_QUIT) {
+                        keep_running = false;
+                    }
+                }
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderClear(renderer);
+                SDL_RenderPresent(renderer);
+            }
 
             // wait two seconds
             SDL_Delay(2000);
